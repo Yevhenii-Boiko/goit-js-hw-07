331f1824237f8f68733f894d139001cb704a13c8
Yevhenii <boiko.yevgenii@gmail.com>
2023-01-06 16:40:47 +0200

    adds 02-gallery solution

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const imagesContainer =
+  document.querySelector('.gallery');
+const imagesMarkup =
+  createGalleryMarkup(galleryItems);
+imagesContainer.insertAdjacentHTML(
+  'beforeend',
+  imagesMarkup
+);
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li>
+            <a class="gallery__item" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}" 
+                    alt="${description}"
+                >
+            </a>
+        </li>`;
+    })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
